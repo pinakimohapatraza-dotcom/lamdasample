1039c3047eafccce94840d3ee98890be28aba030
pinakigitub <pinaki.best@gmail.com>
2026-06-25 22:30:59 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,33 @@
+import json
+
+tasks = []
+
+def lambda_handler(event, context):
+    path = event.get("resource")
+    method = event.get("httpMethod")
+
+    if path == "/health":
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "API is healthy"})
+        }
+
+    if path == "/tasks" and method == "GET":
+        return {
+            "statusCode": 200,
+            "body": json.dumps(tasks)
+        }
+
+    if path == "/tasks" and method == "POST":
+        body = json.loads(event["body"])
+        tasks.append(body)
+
+        return {
+            "statusCode": 201,
+            "body": json.dumps({"message": "Task created"})
+        }
+
+    return {
+        "statusCode": 404,
+        "body": json.dumps({"message": "Not found"})
+    }
